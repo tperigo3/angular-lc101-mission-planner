9eafa62172c1d9e626c524c9beec0e6820dfafaf
Garner Perigo <tperigo3@gmail.com>
2020-11-09 19:21:49 -0600

    finished crew

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,18 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
+  addCrewMember(candidate:object){
+    if (this.crew.includes(candidate)) {
+      this.inCrew = true
+    }else{
+      this.inCrew = false
+    }
 
+    if (this.inCrew){
+      let number = this.crew.indexOf(candidate);
+      this.crew.splice(number, 1);
+    }else if (!this.inCrew && this.crew.length <3){
+      this.crew.push(candidate)
+    }
+  }
 }
